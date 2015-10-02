3c36d4008d633391039fe0d2658f1609b2a8ec34
Craig McKeachie <craigdmckeachie@gmail.com>
2015-10-01 21:10:12 -0400

    Lab 15: Unit Testing Directives

--- app/directives/focus.directive.spec.js
+++ app/directives/focus.directive.spec.js
@@ -1,5 +1,5 @@
 describe('Focus Directive', function() {
-    var createdElement, $rootScope;
+    var createdElement, $rootScope, scope;
 
     //Get the application ready
     beforeEach(module('app'));
@@ -8,32 +8,25 @@ describe('Focus Directive', function() {
     //Write our specs
     beforeEach(inject(function($compile, _$rootScope_) {
         $rootScope = _$rootScope_;
+        scope = $rootScope.$new();
 
         //Creating an createdElement that will call our directive
         //	Looks the same as when we simply write it in our HTML
         createdElement = angular.element(
-            '<input  ng-class="{focus:focused}" type="text" ng-focus="focused=true"  cn-focus value="test" />'
+            '<input name="firstname" ng-model="person.first" cn-focus  type="text"    />'
         );
-
-        $compile(createdElement)($rootScope);
+        spyOn(createdElement[0],'focus');
+        createdElement= $compile(createdElement)(scope);
 
     }));
 
-    xit('tag should contain the directive', function () {
+    it('tag should contain the directive', function () {
         $rootScope.$apply(function(){
-
+            expect(createdElement.attr("cn-focus")).toBeDefined();
         });
-
-        expect(createdElement.html()).toContain("cn-focus");
-
     });
 
-    xit('should get focus when initialized', function() {
-        var scope = createdElement.scope();
-
-        $rootScope.$digest();
-        expect(1).toEqual(1);
-
-
+    it('should call focus when initialized', function() {
+        expect(createdElement[0].focus).toHaveBeenCalled();
     });
 });
